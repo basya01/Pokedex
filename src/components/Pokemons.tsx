88efeaea9f1a5920512340ad135c53f99a76e51e
Arsen <arsen.lulukyan@gmail.com>
2023-03-03 01:52:15 +0200

    add custom scroll

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import { blue } from '@mui/material/colors';
 import React from 'react';
 import { Pokemon } from '../models';
 import { PokemonCard } from './PokemonCard';
@@ -11,7 +12,28 @@ interface PokemonsProps {
 
 export const Pokemons: React.FC<PokemonsProps> = ({ pokemons, onClickPokemon, selected }) => {
   return (
-    <Grid sx={{ maxWidth: '70%', height: 'calc(100vh - 310px)', overflow: 'auto' }} container spacing={2}>
+    <Grid
+      sx={{
+        maxWidth: '70%',
+        height: 'calc(100vh - 310px)',
+        overflow: 'auto',
+        '&::-webkit-scrollbar': {
+          width: '10px',
+          height: '3px',
+        },
+        '&::-webkit-scrollbar-track': { background: 'rgba(0, 0, 0, 0.05)', borderRadius: '20px' },
+        '&::-webkit-scrollbar-thumb': {
+          background: blue[700],
+          borderRadius: '20px',
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          background: '#555',
+        },
+      }}
+      rowGap={2}
+      columnSpacing={2}
+      container
+    >
       {pokemons.map((pokemon) => (
         <Grid item key={pokemon.id} xl={3}>
           <PokemonCard
